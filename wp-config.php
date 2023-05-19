3057cf09b4f27e179cf1d8a1dfc68b3a7d5f8eff
Юрий <htmlcoder001@gmail.com>
2023-05-19 03:11:26 +0300

    Local config settings

--- wp-config.php
+++ wp-config.php
@@ -83,7 +83,8 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+define('WP_HOME','http://l-studio/');
+define('WP_SITEURL','http://l-studio/');
 
 /* That's all, stop editing! Happy publishing. */
 
